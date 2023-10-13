f4b9325e1ba78c97eee85ca468d38e98d1b8c076
sidiAbdalla <sidabdallard@gmail.com>
2023-10-13 17:01:37 +0100

    Fri Oct 13 05:01:37 PM +01 2023

--- src/main/java/ma/enset/AppWrite.java
+++ src/main/java/ma/enset/AppWrite.java
@@ -1,50 +1,12 @@
 package ma.enset;
-
-/**
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-public class AppRead {
-    public static void main(String[] args) throws IOException {
-        Configuration configration = new Configuration();
-        configration.set("fs.defaultFs","hdfs://localhost:9870");
-        FileSystem fileSystem = FileSystem.get(configration);
-//        Path path = new Path( "/home/msa/text.txt");
-        Path path = new Path( "/BDCC/CPP/Cours/CoursCPP1");
-        FSDataInputStream fsDataInputStream = fileSystem.open(path);
-        BufferedReader bufferedReader = new BufferedReader(new
-                InputStreamReader(fsDataInputStream, StandardCharsets.UTF_8));
-        String line = null;
-        while ((line=bufferedReader.readLine())!=null){
-            System.out.println(line);
-        }
-        fsDataInputStream.close();
-        fileSystem.close();
-    }
-}
- **/
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.FSDataOutputStream;
-
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.FSDataOutputStream;
-
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
-import java.util.UUID;
-
 public class AppWrite {
 
     public static void main(String[] args) {
